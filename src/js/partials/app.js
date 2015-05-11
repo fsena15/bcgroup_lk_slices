c7dba6efd77d00dd8378fa62ccc13e77f69b9cdd
Shnell Pavel <shnellpavel@gmail.com>
2015-05-12 01:34:21 +0600

    header (bad icons)

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,43 @@
 /**
  * Created by shnell on 16.04.15.
  */
+$(function () {
+    // categories menu
+    var menuItemWidth = 200,
+        $menuItemsList = $('.categories-navbar .menu-items'),
+        menuItemsCount = $menuItemsList.find('> li').size(),
+        slideTrigger = false;
+
+    $menuItemsList.width(menuItemsCount * menuItemWidth + 100);
+    $('.categories-navbar .left-arrow').on('click', function() {
+        if (!slideTrigger && (parseInt($menuItemsList.css('left')) > -((menuItemsCount - 1) * menuItemWidth) || $menuItemsList.css('left') == 'auto')) {
+            slideTrigger = true;
+            $menuItemsList.animate({
+                left: "-=" + menuItemWidth
+            }, 500, function() {
+                slideTrigger = false;
+            });
+        }
+    });
+
+    $('.categories-navbar .right-arrow').on('click', function() {
+        if (!slideTrigger && parseInt($menuItemsList.css('left')) < 0) {
+            slideTrigger = true;
+            $menuItemsList.animate({
+                left: "+=" + menuItemWidth
+            }, 500, function () {
+                slideTrigger = false;
+            });
+        }
+    });
+
+    //quick menu
+    var topPosition = 357 + 120;
+    $(document).scroll(function () {
+        if ($(this).scrollTop() - topPosition > 0) {
+            $('.right-quick-menu').addClass('dark');
+        } else {
+            $('.right-quick-menu').removeClass('dark');
+        }
+    });
+});
